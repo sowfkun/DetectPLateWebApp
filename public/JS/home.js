18cc82e84cd40f25bd825a3c11914efe16794a41
truongnguyen <dangtruong@tuoitresoft.com>
2021-05-20 22:06:30 +0700

    abc

--- public/JS/home.js
+++ public/JS/home.js
@@ -3,5 +3,19 @@ navbarHeight = $(".navbar").height() + 16;
 $("#main-layout").css("height", $("body").height() - navbarHeight + "px");
 
 // Set height for table of vehicle
-titleHeight  = $(".title").height();
-$(".list-detected-vehicle").css("height", $(".group").height() - titleHeight - 40 + "px");
+titleHeight = $(".title").height();
+$(".table-result-area").css("height", $(".group").height() - titleHeight - 40 + "px");
+
+// Set height for body of table
+headerHeight = $(".table-header").height();
+$(".table-body").css("height", $(".table-result-area").height() - headerHeight + "px");
+
+// zoom image when hover
+$("#img-area img").hover(function () {
+    console.log("hover")
+    $(this).css({ "transform": "scale(1.7)", "transform-origin": "top right"});
+}, function () {
+    $(this).css({ "transform": "scale(1)", "margin-left":"0"});
+})
+
+
