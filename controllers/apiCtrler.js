c0d8161b77c366a95ac1c1012d7097162013ecbc
sowfkun <17521184@gm.uit.edu.vn>
2021-05-28 16:02:27 +0700

    update

--- controllers/apiCtrler.js
+++ controllers/apiCtrler.js
@@ -1,6 +1,20 @@
 module.exports.ProcessDataFromEdge = function (req, res) {
     console.log(req.body);
     console.log(req.files);
-    res.writeHead(200, { 'Content-Type': 'application/json' }); 
+
+    listPlate = req.body.listPlate;
+
+    if (typeof (listPlate) == "string")
+        CheckPlateAndCreateDocument(listPlate);
+    else
+        listPlate.forEach(plate => {
+            CheckPlateAndCreateDocument(plate);
+        });
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end("done");
+}
+
+function CheckPlateAndCreateDocument(plate) {
+
 }
